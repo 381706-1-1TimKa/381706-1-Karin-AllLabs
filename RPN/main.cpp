f3df87fc806ff3cd728a2e3bcd17250c5973d0d3
Timofey <karin.timofej@yandex.ru>
2019-02-17 21:25:03 +0300

    RPN

--- RPN/main.cpp
+++ RPN/main.cpp
@@ -19,7 +19,8 @@ int main()
 	cout << "3 * 2 * (45 - 31) / 6 + (56 - (2 + 8) / 2)" << endl;
 #endif
 
-	TQueue<char> q = StrToRPN(str);
+	TQueue<char> q(3 * str.GetLength());
+	q = StrToRPN(str);
 	double res;
 	TQueue<char> q1(q);
 	while (!q1.IsEmpty())
